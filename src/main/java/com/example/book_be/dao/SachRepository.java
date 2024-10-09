4cab89ca677d68f296efbaf22a7b2b8e4dfe5e54
tienvovan917 <tienvovan917@gmail.com>
2024-10-09 16:32:00 +0700

    910

--- src/main/java/com/example/book_be/dao/SachRepository.java
+++ src/main/java/com/example/book_be/dao/SachRepository.java
@@ -2,10 +2,14 @@ package com.example.book_be.dao;
 
 import com.example.book_be.entity.HinhAnh;
 import com.example.book_be.entity.Sach;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RepositoryRestResource(path = "sach")
 public interface SachRepository extends JpaRepository<Sach, Long> {
+    Page<Sach> findByTenSachContaining(@RequestParam("tensach") String tenSach, Pageable pageable);
 }
